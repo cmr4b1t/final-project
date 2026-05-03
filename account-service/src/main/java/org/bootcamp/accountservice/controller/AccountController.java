c34670c5762cde97449e05bc305bc853e03e1689
dfsaturno <dfsaturno@stefanini.com>
2026-05-03 01:29:41 -0500

    accounts based 2

---
+++ account-service/src/main/java/org/bootcamp/accountservice/controller/AccountController.java
@@ -0,0 +1,13 @@
+package org.bootcamp.accountservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/accounts")
+@RequiredArgsConstructor
+@Validated
+public class AccountController {
+}
